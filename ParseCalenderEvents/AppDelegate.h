c36966e10b2ff6948c990f5e8e15f9174a76a861
github username <github email address>
2014-06-30 08:24:01 -0400

    initial commit

---
+++ ParseCalenderEvents/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParseCalenderEvents
+//
+//  Created by Troy Barrett on 6/30/14.
+//  Copyright (c) 2014 OB_Troy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
